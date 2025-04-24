1fd544be45efcf0928075a3fed2fe822681b1309
Huballs <ballshugo@yandex.ru>
2025-04-24 13:01:55 +0300

    new file:   ids_5000.txt 	modified:   src/device_payloads.cpp 	modified:   src/device_payloads.hpp

--- src/device_payloads.hpp
+++ src/device_payloads.hpp
@@ -6,10 +6,9 @@
 
 using payload_t = std::vector<uint8_t>;
 
-extern payload_t ws_payload_settings_probes_1;
-extern payload_t ws_payload_settings_probes_2;
-extern payload_t ws_payload;
-
+// extern payload_t ws_payload_settings_probes_1;
+// extern payload_t ws_payload_settings_probes_2;
+// extern payload_t ws_payload;
 
 // return payload based on value of request
 std::optional<std::vector<uint8_t>>  ws_get_payload (std::string request);
